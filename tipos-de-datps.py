fa5b3e023671412eea3a1940f77133b3957fa67c
andres naranjo <santo2828@gmail.com>
2024-09-06 20:43:23 -0500

    tipos de datos python

---
+++ tipos-de-datps.py
@@ -0,0 +1,25 @@
+"string" # cadena de caracteres
+'char' # caracter
+'bool' # booleano
+'byte' # byte
+
+"""
+tus datos son:
+
+ nombre: Andres Arias
+"""
+'''
+tus datos son:
+
+ nombre: Andres Arias
+ 
+ '''
+
+ #En python tenemos diferentes tipos de numeros
+ #4 # entero
+40.2#flotante
+4j # complejo
+
+#Tipos de datos boolean
+True # verdadero
+False # falso
