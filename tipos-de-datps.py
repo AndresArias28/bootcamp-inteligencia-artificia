e4f0e33b56cf0244d1711a0e5f5160b83e2f9a06
andres naranjo <santo2828@gmail.com>
2024-09-13 09:53:22 -0500

    varaibles en python

--- tipos-de-datps.py
+++ tipos-de-datps.py
@@ -22,3 +22,47 @@ nombre: Andres Arias
 #Tipos de datos boolean
 True # verdadero
 False # falso
+
+#Tipos de datos list
+[1,2,3] # lista
+[1,2,3,4,5,6,7,8,9,10] # lista
+
+#Tipos de datos tuplas
+(1,2,3) # tupla
+(1,2,3,4,5,6,7,8,9,10) # tupla
+
+#Tipos de datos diccionario
+{'nombre':'Andres','edad':25} # diccionario
+
+#Tipos de datos set
+{1,2,3} # set
+{1,2,3,4,5,6,7,8,9,10} # set
+
+#Tipos de datos frozenset
+frozenset({1,2,3}) # frozenset
+
+#Tipos de datos range
+range(1,10) # range
+
+#Tipos de datos bytes
+bytes(1) # bytes
+
+#Tipos de datos bytearray
+bytearray(1) # bytearray
+
+#Tipos de datos memoryview
+memoryview(1) # memoryview
+
+#Tipos de datos complex
+1+2j # complex
+
+#Tipos de datos None
+None # None     
+
+#Tipos de datos Ellipsis
+Ellipsis # Ellipsis
+
+#Tipos de datos NotImplemented
+NotImplemented # NotImplemented
+
+
---
+++ varaibles.py
@@ -0,0 +1,20 @@
+numero = 10
+numero = 10 +1
+numero += 1 
+
+print(numero)
+
+num = 5
+num += 5 # 10 mas 5 es iguala 15
+num -= 5 # 10 menos 5 es iguala 5
+
+
+nombre = 'Andres'
+bienvenido = 'Hola ' + nombre + ' feliz noche'
+
+#otra forma
+bienvenido = f"Hola {nombre} feliz noche" 
+print(bienvenido)
+
+print("ola" in bienvenido) #sirve para saber si una cadena se encuentra dentro de otra cadena
+print ("ola" not in bienvenido) # sirve para saber si una cadena no se encuentra dentro de otra cadena
